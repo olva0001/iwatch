361d1a68037ac39d18f97fdcc054a9cf8b72cbd0
Oliver Valter <olva0001@kea.stud.dk>
2025-04-03 13:19:39 +0200

    YEAH!!!

--- src/components/Mainbox.jsx
+++ src/components/Mainbox.jsx
@@ -1,4 +1,5 @@
 "use client";
+import Threedots from "./Threedots";
 
 const Mainbox = () => {
     return (
@@ -11,8 +12,8 @@ const Mainbox = () => {
             <div className="col-start-2 row-start-1 mt-12 flex">
                 <img className="w-full scale-90" src="/images/5.png" alt="test" />
             </div>
-            <div className="col-start-3 mt-56">
-                <img src="/images/Colors.png" alt="" />
+            <div className="col-start-3 mt-56 mr-5">
+                <Threedots />
             </div>
         </section>
     )
---
+++ src/components/Threedots.jsx
@@ -0,0 +1,15 @@
+"use client";
+
+const Threedots = () => {
+    return (
+            <div className="flex flex-col items-center">
+              <span className="w-[30px] h-[30px] rounded-[2rem] bg-[#434558] cursor-pointer border-white border-[3px] scale-150"></span>
+                <div className="w-[2px] h-[30px] bg-[repeating-linear-gradient(to_bottom,white_0px,white_2px,transparent_2px,transparent_6px)]"></div>
+              <span className="w-[30px] h-[30px] bg-[#6ADDCC] rounded-[2rem] cursor-pointer border-white border-[3px]"></span>
+                <div className="w-[2px] h-[30px] bg-[repeating-linear-gradient(to_bottom,white_0px,white_2px,transparent_2px,transparent_6px)]"></div>
+              <span className="w-[30px] h-[30px] bg-[#f9cdc480] rounded-full cursor-pointer border-white border-[3px]"></span>
+            </div>
+    )
+}
+
+export default Threedots;
